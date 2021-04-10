54d3241a0126099726a0488ad06c28c44aeb7118
Aldiyar <adelov.aldiyar@gmail.com>
2021-04-11 02:20:07 +0600

    lab9

---
+++ lab9/hhback/api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Company, Vacancy
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'city', 'address')
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'salary', 'company')
